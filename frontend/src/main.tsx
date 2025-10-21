bbb0a51261bae2d8b471089a61f2c104346434d0
dangvanphuc <dangvanphuc1710@gmail.com>
2025-10-21 22:48:12 +0700

    bai moii  Jenkins

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Login from "./Login";
-import Register from "./Register";
-import Products from "./Products";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
